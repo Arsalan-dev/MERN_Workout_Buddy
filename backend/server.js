8336775668983a21a29beb490fd98ae93bcfed79
Syed Arsalan <sya@4fo.de>
2023-05-28 14:45:26 +0200

    Express router, and dummy routes added

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const workoutRoutes = require('./routes/workouts')
 
 // express app
 const app = express();
@@ -14,9 +15,7 @@ app.use((req, res, next) => {
 });
 
 //routes
-app.get("/", (req, res) => {
-  res.json({ msg: "GET request" });
-});
+app.use(workoutRoutes)
 
 // listen to requests
 app.listen(process.env.PORT, () => {
