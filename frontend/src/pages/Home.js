97640269ae80c3c32746aca1d45a5691bf2fee46
Syed Arsalan <sya@4fo.de>
2023-05-29 00:43:00 +0200

    new form added to create new workouts

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 
-
 //components
-import WorkoutDetails from '../components/WorkoutDetails';
+import WorkoutDetails from "../components/WorkoutDetails";
+import WorkoutForm from "../components/WorkoutForm";
 
 const Home = () => {
   const [workouts, setWorkouts] = useState(null);
@@ -24,10 +24,11 @@ const Home = () => {
     <div className="home">
       <div className="workouts">
         {workouts &&
-          workouts.map((workout) => ( 
-          <WorkoutDetails key={workout._id} workout={workout} />
+          workouts.map((workout) => (
+            <WorkoutDetails key={workout._id} workout={workout} />
           ))}
       </div>
+      <WorkoutForm />
     </div>
   );
 };
